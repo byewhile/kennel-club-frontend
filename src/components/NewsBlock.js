99fbf4a1e6ea68a687f5c8eb7cb9535e733865db
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-11 12:15:19 +0300

    delete check info

--- src/components/NewsBlock.js
+++ src/components/NewsBlock.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function NewsBlock({ newsInfo }) {
-    console.log(newsInfo.image);
     return (
         <div className="bg-white rounded-lg shadow-md overflow-hidden h-full flex flex-col">
             <div className="relative h-100">
