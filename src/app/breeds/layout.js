ce1a096cebb17dd1a01b00e8927138c301c183c6
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-13 17:24:38 +0300

    add pages layout

---
+++ src/app/breeds/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "Породы | Клуб собаководства",
+    description: "Породы | Клуб собаководства",
+};
+
+export default function BreedsLayout({ children }) {
+    return children;
+}
---
+++ src/app/news/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "Новости | Клуб собаководства",
+    description: "Новости | Клуб собаководства",
+};
+
+export default function NewsLayout({ children }) {
+    return children;
+}
