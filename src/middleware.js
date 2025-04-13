6fe295c1579d57d8186a1267f34cf6231fc6fce3
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-13 17:24:05 +0300

    add middleware

---
+++ src/middleware.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+export async function middleware(request) {
+  const { pathname } = request.nextUrl;
+
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/checkSession.php`, {
+      headers: {
+        "Cookie": request.cookies.toString()
+      }
+    });
+
+    const data = await response.json();
+    const isAuthenticated = data.authenticated;
+
+    if (isAuthenticated && pathname === "/auth") {
+      return NextResponse.redirect(new URL("/profile", request.url));
+    }
+
+    if (!isAuthenticated && pathname === "/profile") {
+      return NextResponse.redirect(new URL("/auth", request.url));
+    }
+  } catch (err) {
+    if (pathname === "/profile") {
+      return NextResponse.redirect(new URL("/auth", request.url));
+    }
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/profile", "/auth"]
+}
