e5da549fca8f5739057385452535d4ec24b67709
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-13 17:26:36 +0300

    update auth page

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,5 +1,19 @@
+"use client"
+
+import LoginForm from "@/components/LoginForm";
+import RegisterForm from "@/components/RegisterForm";
+import { useState } from "react";
+
 export default function AuthPage() {
+    const [loginForm, setLoginForm] = useState(true);
+
     return (
-        <div>Авторизация</div>
+        <div className="flex flex-col items-center justify-center h-[75vh]">
+            {loginForm ? <LoginForm /> : <RegisterForm />}
+            <div className="flex flex-col items-center mt-3">
+                {loginForm ? "Нет аккаунта?" : "Есть аккаунт?"}
+                <button className="text-green font-bold cursor-pointer" onClick={() => setLoginForm(!loginForm)}>{loginForm ? "Зарегистрироваться" : "Войти"}</button>
+            </div>
+        </div>
     )
 }
