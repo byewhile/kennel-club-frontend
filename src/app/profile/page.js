49beeeba09f0373d767ea1e260779b188f5f5cd8
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-13 17:25:53 +0300

    change profile page for test

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,5 +1,22 @@
+"use client"
+
+import axios from "axios";
+
 export default function ProfilePage() {
+    const logout = async () => {
+        try {
+            await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/logout.php`, {
+                withCredentials: true
+            });
+            window.location.reload();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
-        <div>Профиль</div>
+        <div>
+            <button onClick={logout}>Выйти</button>
+        </div>
     )
 }
