d617dcf3acbe5e78e7c4a416640df4092d1708c3
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-13 17:25:10 +0300

    fix

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,6 +1,6 @@
 export default function LoadingSpinner() {
     return (
-        <div className="flex justify-center items-center h-100">
+        <div className="flex justify-center items-center">
             <div className="animate-spin rounded-full h-15 w-15 border-t-3 border-green" />
         </div>
     )
