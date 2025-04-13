07bc5594c358e9d9110e5b1ae63a66d4cfa29998
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-13 18:22:16 +0300

    fix captcha

--- src/components/CaptchaBlock.js
+++ src/components/CaptchaBlock.js
@@ -1,17 +1,25 @@
 "use client"
 
+import axios from "axios";
 import { useEffect, useState } from "react"
 
 export default function CaptchaBlock() {
     const [captchaUrl, setCaptchaUrl] = useState(null);
 
     useEffect(() => {
-        const createCaptcha = () => {
+        const createCaptcha = async () => {
             try {
-                const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/createCaptcha.php`;
+                const res = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/createCodeForCaptcha.php`, {
+                    withCredentials: true
+                });
+                const code = res.data.code;
+
+                const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/createCaptcha.php?code=${code}`;
                 setCaptchaUrl(url);
             } catch (err) {
                 console.log(err);
+            } finally {
+                
             }
         }
         createCaptcha();
