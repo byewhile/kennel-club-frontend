ccc318c3d874e6e55cb595be3e992d96916aad4b
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-13 17:24:56 +0300

    fix

--- src/components/ErrorBlock.js
+++ src/components/ErrorBlock.js
@@ -1,7 +1,7 @@
 export default function ErrorBlock({ error }) {
     return (
-        <div className="flex justify-center items-center h-100">
-            <div className="text-red-500 text-xl font-bold">{error}</div>
+        <div className="flex justify-center items-center">
+            <div className="text-red-500 text-lg font-bold">{error}</div>
         </div>
     )
 }
