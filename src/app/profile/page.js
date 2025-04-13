8707aaf7c958f2490fa9f19990a055f586c686d2
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-13 22:09:13 +0300

    update profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,12 +1,8 @@
 "use client"
-
+ 
 import axios from "axios";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
+ 
 export default function ProfilePage() {
-    const router = useRouter();
-
     const logout = async () => {
         try {
             await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/logout.php`, {
@@ -17,24 +13,7 @@ export default function ProfilePage() {
             console.log(err);
         }
     }
-
-    useEffect(() => {
-        const checkSession = async () => {
-            try {
-                const res = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/checkSession.php`, {
-                    withCredentials: true
-                });
-
-                if (!res.data.authenticated) {
-                    router.push("/auth");
-                }
-            } catch (err) {
-                console.log(err);
-            }
-        }
-        checkSession();
-    }, []);
-
+ 
     return (
         <div>
             <button onClick={logout}>Выйти</button>
