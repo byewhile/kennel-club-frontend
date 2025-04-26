8919aeef4f8699883919e120032ee41b81bbe88b
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-26 16:40:59 +0300

    update detailsBlock for info about breed

--- src/components/DetailsBlock.js
+++ src/components/DetailsBlock.js
@@ -1,8 +1,18 @@
 import Image from "next/image";
+import { FaDog, FaWeightHanging  } from "react-icons/fa6";
+import { MdHeight } from "react-icons/md";
+import { GiComb } from "react-icons/gi";
+import NothingBlock from "./NothingBlock";
 
-export default function DetailsBlock({ details }) {
+export default function DetailsBlock({ details, about }) {
+    if (!details) {
+        return (
+            <NothingBlock />
+        );
+    }
+
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 mt-8 gap-5">
+        <div className="grid grid-cols-1 lg:grid-cols-2 mt-4 gap-5">
             <div>
                 <Image 
                     src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/images/${details.image}`} 
@@ -15,10 +25,78 @@ export default function DetailsBlock({ details }) {
             </div>
         
             <div>
-                <h2 className="text-green font-bold text-xl lg:text-3xl my-5">{details.title}</h2>
-                <div className="flex flex-col gap-5 text-lg text-gray-600 mb-4">
-                    <p>{details.description}</p>
+                <div className="flex items-end justify-between mb-3">
+                    <h2 className="text-green font-bold text-xl lg:text-3xl">{details.title}</h2>
+                    {about == "news" && (
+                        <span className="text-gray-400 font-medium">{details.date.split("-").reverse().join(".")}</span>
+                    )}
                 </div>
+                
+                {about == "news" ? (
+                    <div className="text-lg text-gray-600 my-3">
+                        <p>{details.description}</p>
+                    </div>
+                ) : (
+                    <div>
+                        <div>
+                            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+                                <div className="flex items-center rounded-lg shadow-md px-5 py-10 gap-5">
+                                    <div className="text-4xl text-green">
+                                        <FaDog />
+                                    </div>
+
+                                    <div className="font-semibold">
+                                        <div className="text-gray-500">Длительность жизни</div>
+                                        <div>
+                                            От <span>{details.description.age.from}</span> до <span>{details.description.age.to}</span> лет
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="flex items-center rounded-lg shadow-md px-5 py-10 gap-5">
+                                    <div className="text-4xl text-green">
+                                        <GiComb />
+                                    </div>
+
+                                    <div className="font-semibold">
+                                        <div className="text-gray-500">Шерсть</div>
+                                        <div>{details.description.wool}</div>
+                                    </div>
+                                </div>
+
+                                <div className="flex items-center rounded-lg shadow-md px-5 py-10 gap-5">
+                                    <div className="text-4xl text-green">
+                                        <MdHeight />
+                                    </div>
+
+                                    <div className="font-semibold">
+                                        <div className="text-gray-500">Рост в холке, см</div>
+                                        <div>
+                                            От <span>{details.description.height.from}</span> до <span>{details.description.height.to}</span> см
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="flex items-center rounded-lg shadow-md px-5 py-10 gap-5">
+                                    <div className="text-4xl text-green">
+                                        <FaWeightHanging />
+                                    </div>
+
+                                    <div className="font-semibold">
+                                        <div className="text-gray-500">Вес, кг</div>
+                                        <div>
+                                            От <span>{details.description.weight.from}</span> до <span>{details.description.weight.to}</span> кг
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="text-lg text-gray-600 my-3">
+                            <p>{details.description.text}</p>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     )
