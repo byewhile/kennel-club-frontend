82fd17151faa964aa855172786f923cdda02045b
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-26 16:41:12 +0300

    update news block

--- src/components/NewsBlock.js
+++ src/components/NewsBlock.js
@@ -2,8 +2,12 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function NewsBlock({ newsInfo }) {
+    let description = newsInfo.description;
+    description = description.split(".");
+    description = description[0];
+
     return (
-        <div className="bg-white rounded-lg shadow-md overflow-hidden h-full flex flex-col">
+        <Link href={`/news/${newsInfo.id}`} className="rounded-lg overflow-hidden shadow-md h-[600px]">
             <div className="relative h-100">
                 <Image 
                     src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/images/${newsInfo.image}`} 
@@ -11,18 +15,16 @@ export default function NewsBlock({ newsInfo }) {
                     fill
                     className="object-cover"
                     loading="lazy"
-                    sizes="(max-width: 767px) 100vw, (max-width: 1023px) 50vw, 33vw"
                 />
             </div>
 
             <div className="p-6">
-                <h3 className="text-green font-bold text-xl mb-3">{newsInfo.title}</h3>
-                <p>{newsInfo.description}</p>
+                <div className="flex items-end justify-between gap-3 mb-3">
+                    <h3 className="text-green font-bold text-xl">{newsInfo.title}</h3>
+                    <span className="text-gray-400 font-medium">{newsInfo.date.split("-").reverse().join(".")}</span>
+                </div>
+                <p>{description}...</p>
             </div>
-              
-            <div className="p-6">
-                <Link href={`/news/${newsInfo.id}`} className="text-green bg-lime rounded-lg p-3 font-medium">Подробнее</Link>
-            </div>
-        </div>
+        </Link>
     )
 }
