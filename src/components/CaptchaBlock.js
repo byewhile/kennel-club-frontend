1e917ff260545d167a0864e14c4b93138ea36396
byewhile <93428940+byewhile@users.noreply.github.com>
2025-04-30 13:52:52 +0300

    add feature to reload captcha

--- src/components/CaptchaBlock.js
+++ src/components/CaptchaBlock.js
@@ -1,29 +1,35 @@
 "use client"
  
 import { useEffect, useState } from "react"
+import { FaArrowsRotate } from "react-icons/fa6";
  
 export default function CaptchaBlock() {
     const [captchaUrl, setCaptchaUrl] = useState(null);
+
+    const createCaptcha = () => {
+        try {
+            const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/createCaptcha.php?q=${Math.random()}`;
+            setCaptchaUrl(url);
+        } catch (err) {
+            console.log(err);
+        }
+    }
  
     useEffect(() => {
-        const createCaptcha = () => {
-            try {
-                const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/createCaptcha.php`;
-                setCaptchaUrl(url);
-            } catch (err) {
-                console.log(err);
-            }
-        }
         createCaptcha();
-     }, []);
+    }, []);
  
     return (
         <div>
             {captchaUrl && (
-                <img
-                    src={captchaUrl}
-                    alt="Captcha"
-                />
+                <div className="flex items-center gap-2 text-xl select-none">
+                    <img
+                        src={captchaUrl}
+                        alt="Captcha"
+                        className="rounded-lg"
+                    />
+                    <FaArrowsRotate className="cursor-pointer" onClick={createCaptcha} />
+                </div>
             )}
         </div>
     )
