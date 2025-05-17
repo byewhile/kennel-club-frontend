0e092ad9aa06fbac94e6154bed3967e8c3e09fed
byewhile <93428940+byewhile@users.noreply.github.com>
2025-05-17 20:33:33 +0300

    change style error block

--- src/components/ErrorBlock.js
+++ src/components/ErrorBlock.js
@@ -1,7 +1,7 @@
 export default function ErrorBlock({ error }) {
     return (
-        <div className="flex justify-center items-center text-center mt-5">
-            <div className="text-red-500 text-lg font-bold">{error}</div>
+        <div className="flex justify-center items-center text-center">
+            <div className="text-red-500 text-lg font-semibold">{error}</div>
         </div>
     )
 }
