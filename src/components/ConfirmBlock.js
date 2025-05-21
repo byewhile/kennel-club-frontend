b82f7d35736f8828ab868dd70add7edb935b2be6
byewhile <93428940+byewhile@users.noreply.github.com>
2025-05-21 22:31:43 +0300

    fix

--- src/components/ConfirmBlock.js
+++ src/components/ConfirmBlock.js
@@ -1,32 +1,34 @@
-const ACTION_CONFIG = {
-    delete: {
-        doText: "удалить",
-        color: "red-400",
-        who: {
-            users: "пользователя",
-            friends: "друга"
-        }
-    },
-    make_admin: {
-        doText: "сделать администратором",
-        color: "yellow-400",
-        who: {
-            users: "пользователя"
+export default function ConfirmBlock({ actionType, entityType, action, setIsShowConfirmBlock }) {
+    const ACTION_CONFIG = {
+        delete: {
+            doText: "удалить",
+            border_color: "border-red-400",
+            text_color: "text-red-400",
+            who: {
+                users: "пользователя",
+                dogs: "друга"
+            }
+        },
+        make_admin: {
+            doText: "сделать администратором",
+            border_color: "border-yellow-400",
+            text_color: "text-yellow-400",
+            who: {
+                users: "пользователя"
+            }
         }
     }
-}
 
-export default function ConfirmBlock({ actionType, entityType, action, setIsShowConfirmBlock }) {
     const config = ACTION_CONFIG[actionType];
     const who = config.who[entityType];
 
     return (
         <div className="fixed inset-0 z-10 backdrop-blur">
             <div className="min-h-full flex items-center justify-center p-4">
-                <div className={`bg-white border-2 border-${config.color} p-5 rounded-xl font-semibold`}>
-                    <div className="text-lg lg:text-xl text-center">Вы точно хотите <span className={`text-${config.color}`}>{config.doText}</span> этого {who}</div>
+                <div className={`bg-white border-2 ${config.border_color} p-5 rounded-xl font-semibold`}>
+                    <div className="text-lg lg:text-xl text-center">Вы точно хотите <span className={`${config.text_color}`}>{config.doText}</span> этого {who}</div>
                     <div className="flex justify-evenly mt-5">
-                        <button className={`cursor-pointer text-${config.color}`} onClick={action}>{config.doText.charAt(0).toUpperCase() + config.doText.slice(1)}</button>
+                        <button className={`cursor-pointer ${config.text_color}`} onClick={action}>{config.doText.charAt(0).toUpperCase() + config.doText.slice(1)}</button>
                         <button className="cursor-pointer text-green" onClick={() => setIsShowConfirmBlock(false)}>Отмена</button>
                     </div>
                 </div>
