fcc54be806d44f9d6c6d9a12057b3a354fdbd674
byewhile <93428940+byewhile@users.noreply.github.com>
2025-05-21 20:34:25 +0300

    fix

--- src/components/ConfirmDeleteDogBlock.js
+++ src/components/ConfirmDeleteDogBlock.js
@@ -1,12 +1,12 @@
-export default function ConfirmBlock({ action, setIsShowConfirmBlock }) {
+export default function ConfirmDeleteDogBlock({ action, setIsShowConfirmBlock }) {
     return (
-        <div className="h-full w-full top-0 left-0 absolute z-100 backdrop-blur">
-            <div className="h-full flex items-center justify-center">
+        <div className="fixed inset-0 z-10 backdrop-blur">
+            <div className="min-h-full flex items-center justify-center p-4">
                 <div className="bg-white border-2 border-red-400 p-5 rounded-xl font-semibold">
                     <div className="text-lg lg:text-xl">Вы точно хотите <span className="text-red-400">удалить</span> этого друга?</div>
                     <div className="flex justify-evenly mt-5">
                         <button className="cursor-pointer text-red-400" onClick={action}>Удалить</button>
-                        <button className="cursor-pointer text-green" onClick={() => setIsShowConfirmBlock(prev => !prev)}>Отмена</button>
+                        <button className="cursor-pointer text-green" onClick={() => setIsShowConfirmBlock(false)}>Отмена</button>
                     </div>
                 </div>
             </div>
